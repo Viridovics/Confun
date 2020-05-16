6ac1e3464496ce46b07933ef515e6ebd67535782
Eugene <m_ec@mail.ru>
2020-05-16 17:33:29 +0300

    Fix fake script

--- fake.fsx
+++ fake.fsx
@@ -26,35 +26,44 @@ let dotnetSdkVersion = "3.1.101"
 let installedSdk = lazy (DotNet.install (fun cfg -> { cfg with Version = DotNet.CliVersion.Version dotnetSdkVersion }))
 
 // Set general properties without arguments
+let inline compileWithoutArgs arg = DotNet.Options.lift installedSdk.Value arg
+
 let inline compileWithProject projectPath =
     let projectOption = sprintf "--project %s" projectPath
     DotNet.Options.lift installedSdk.Value >> DotNet.Options.withCustomParams (Some projectOption)
+
+let inline compileWithProjectPathAndFsxWorkingDirectory projectPath =
+    let projectOption = sprintf "--project %s" projectPath
+    DotNet.Options.lift installedSdk.Value 
+            >> DotNet.Options.withWorkingDirectory __SOURCE_DIRECTORY__ 
+            >> DotNet.Options.withCustomParams (Some projectOption)
 
 let inline runTestWithCollectCoverage arg =
     let collectCoverageOption = "/p:CollectCoverage=true /p:CoverletOutputFormat=opencover"
     (DotNet.Options.lift installedSdk.Value >> DotNet.Options.withCustomParams (Some collectCoverageOption)) arg
 
-// Set general properties without arguments
-let inline compileWithoutArgs arg = DotNet.Options.lift installedSdk.Value arg
+
+let confunSlnPath = System.IO.Path.Combine(__SOURCE_DIRECTORY__, "Confun.sln")
+let unitTestsPath = System.IO.Path.Combine(__SOURCE_DIRECTORY__, @"src\test\Confun.UnitTests\Confun.UnitTests.fsproj")
+let testAppPath = System.IO.Path.Combine(__SOURCE_DIRECTORY__, @"src\test\FirstSample\FirstSample.fsproj")
 
 // Default target
-Target.create "Build" (fun _ -> DotNet.build compileWithoutArgs "Confun.sln")
+Target.create "Build" (fun _ -> DotNet.build compileWithoutArgs confunSlnPath)
 
-//TODO with working dir
 Target.create "Run" (fun _ ->
-    let result = DotNet.exec (compileWithProject @".\src\test\FirstSample\FirstSample.fsproj") "run" ""
+    let result = DotNet.exec (compileWithProjectPathAndFsxWorkingDirectory testAppPath) "run" ""
     if (result.ExitCode <> 0) then raise (GenerationErrorException(sprintf "%A" result.Messages)))
 
-Target.create "Restore" (fun _ -> DotNet.restore compileWithoutArgs "Confun.sln")
+Target.create "Restore" (fun _ -> DotNet.restore compileWithoutArgs confunSlnPath)
 
 let artifactsTestsDir  = "./artifacts/tests/"
 
 Target.create "RunTests" (fun _ ->
-    DotNet.test runTestWithCollectCoverage @".\src\test\Confun.UnitTests\Confun.UnitTests.fsproj"
+    DotNet.test runTestWithCollectCoverage unitTestsPath
 )
 
 Target.create "OnlyTests" (fun _ ->
-    DotNet.test runTestWithCollectCoverage @".\src\test\Confun.UnitTests\Confun.UnitTests.fsproj"
+    DotNet.test runTestWithCollectCoverage unitTestsPath
 )
 
 "Restore" 
